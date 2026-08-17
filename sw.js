d6c858eef542773e86eb3155cdac006de0d718a4
oliviernaudinmotion-hash <olivier.naudin.motion@gmail.com>
2026-08-17 11:50:37 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // dans manifest.json. Le nom du cache en découle : changer de version purge
 // donc automatiquement l'ancien cache, ce que "lemmonfit-cache-v1" (figé) ne
 // faisait jamais.
-var VERSION = "1.10.0";
+var VERSION = "1.15.0";
 var CACHE_NAME = "lemmonfit-" + VERSION;
 
 var INDEX_URL = new URL("./index.html", self.location.href).href;
